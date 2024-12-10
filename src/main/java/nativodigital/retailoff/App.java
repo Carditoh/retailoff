60bf57d6c5835cca0a95fb729062cb15867ea745
Carditoh <ricardosepulvedaojeda@outlook.com>
2024-12-10 02:46:19 -0300

    commit inicial

---
+++ src/main/java/nativodigital/retailoff/App.java
@@ -0,0 +1,10 @@
+package nativodigital.retailoff;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
